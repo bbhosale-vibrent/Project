546106732dfdb253c6c810e7cd4bbb280f7350ef
Bhagyashri Bhosale <bhagyashri.bhosale@vignet.com>
2024-03-29 15:18:51 +0530

    added

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import { AppBar, Box, MenuItem, Select, Toolbar, Typography } from '@mui/material'
-import {  useNavigate } from 'react-router-dom'
+import React from "react";
+import {
+  AppBar,
+  Box,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-    const Navigate = useNavigate();
-    const naviagtetologin = () =>{
-       Navigate("/login");
-    }
+  const Navigate = useNavigate();
+  const naviagtetologin = () => {
+    Navigate("/login");
+  };
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-    <Toolbar>
-      <Typography
-        variant="h6"
-        component="div"
-        sx={{ flexGrow: 1, marginLeft: "0ch" }}
-      >
-        Welcome To WebSite
-      </Typography>
-      <Select
-        sx={{ marginLeft: "1ch" }}
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value=""
-        label="Login"
-        onClick={naviagtetologin}
-      >
-        <MenuItem>Sign In</MenuItem>
-      </Select>
-
-     
-    </Toolbar>
-  </AppBar>
-  </Box>
-  </>
-  )
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, marginLeft: "0ch" }}
+            >
+              Welcome To WebSite
+            </Typography>
+            <Select
+              sx={{ marginLeft: "1ch" }}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value=""
+              label="Login"
+              onClick={naviagtetologin}
+            >
+              <MenuItem>Sign In</MenuItem>
+            </Select>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
